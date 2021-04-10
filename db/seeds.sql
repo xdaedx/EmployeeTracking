6916b6b33e153c6b4479b076aff377b10f25b88f
xdaedx <hewittsunisa@gmail.com>
2021-04-10 03:06:14 -0500

    starting seeds before values

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+-- Insert all the values later
+
+USE tracker_db; 
+
+INSERT INTO departments (dpt_name)
+VALUES
+
+INSERT INTO roles (title, salary, dpt_id)
+VALUES
+
+INSERT INTO employees (first_name, last_name, role_id, mgr_id)
+        VALUES
