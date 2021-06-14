2b75631ab5bf4b924f8652e398573146c22a9fd1
xdaedx <hewittsunisa@gmail.com>
2021-06-14 17:47:25 -0500

    not too sure what is wrong but will come back later

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,16 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
-// Fill in sql host and password later
-const connection = mysql.createconnection({
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306, 
+    user: 'root', 
+    password: 'Gates7dSme!i90', 
+    database: 'tracker_db',
 }); 
 
 connection.connect(err => {
     if (err) throw err; 
     console.log('\nConnected as id ' + connection.threadId); 
 }); 
+
+module.exports = connection; 
